32672f21a3a8206c31ebe56d9ee3c658cd9f1413
ritaOmondi <cherylritaomondi@gmail.com>
2024-06-28 09:55:57 +0300

    editing homepage

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -8,4 +8,12 @@ menu.onclick = () =>{
 window.onscroll =()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
+};
+var swiper = new Swiper(".home-slider",{
+    loop:true,
+    navigation:{
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+    },
+});
+
